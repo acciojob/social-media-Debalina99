25137c2b5e4cbe2bd95ab3946b34f0023509302a
Debalina Laha <32841776+Debalina99@users.noreply.github.com>
2025-10-02 20:19:04 +0530

    Update PostsPage.js

--- src/components/PostsPage.js
+++ src/components/PostsPage.js
@@ -1,19 +1,28 @@
 import React from 'react'
-import CreatePostForm from '../shared/CreatePostForm'
-import PostCard from '../shared/PostCard'
+import CreatePostForm from './CreatePostForm'
+import PostCard from './PostCard'
 
-
-export default function PostsPage({ state, createPost, reactToPost }){
+export default function PostsPage({ state, createPost, reactToPost }) {
   return (
     <section>
-    <div className="top-controls">Top Controls</div>
-    <CreatePostForm users={state.users} onSubmit={createPost} />
-    <div className="posts-list">
-      <div className="posts-header">Posts header</div>
-      {state.posts.slice(1).map(p => (
-      <PostCard key={p.id} post={p} users={state.users} reactToPost={reactToPost} />
-      ))}
-    </div>
+      <div className="top-controls">Top Controls</div>
+
+      <CreatePostForm users={state.users} onSubmit={createPost} />
+
+      <div className="posts-list">
+        {/* First child should be a placeholder/header */}
+        <div className="posts-header">Posts header</div>
+
+        {/* Render ALL posts in order */}
+        {state.posts.map((p) => (
+          <PostCard
+            key={p.id}
+            post={p}
+            users={state.users}
+            reactToPost={reactToPost}
+          />
+        ))}
+      </div>
     </section>
-)
+  )
 }
